144dcc04bb85b15ec6ba313fba6e469319bc11b9
Harsha <harshagihan658@gmail.com>
2020-10-03 20:44:57 +0530

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
 import './App.css';
 import Navbar from "./component/navbar";
 import home from './component/home';
+import Contactus from './component/contactus';
 import './App.css';
 
 function App() {
@@ -13,23 +13,11 @@ function App() {
       <Switch>
         <Route exact path= '/home'component={ home } />
         <Route exact path='/navbar' component={ Navbar } />
+        <Route exact path='/contactus' component={ Contactus } />
       </Switch>
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
 
-        </p>
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
       </BrowserRouter>
