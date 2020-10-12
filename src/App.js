2e29cfb9bb5a83ee4cb54be7c7f1dc44bcfb247b
NethminiSAT <thimanethmini@gmail.com>
2020-10-12 19:23:36 +0530

    coupon and feedback pages

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import about_us from "./component/about_us";
 import product from "./component/product";
 import productPage from "./component/productPage";
 import register from "./component/register";
+import feedback from "./component/feedback"
+import coupon from "./component/coupon"
 
 function App() {
   return (
@@ -18,12 +20,15 @@ function App() {
       <Switch>
         <Route exact path= '/home'component={ home } />
         <Route exact path='/navbar' component={ Navbar } />
+        <Route exact path= '/register' component={ register } />
+        <Route exact path= '/feedback' component={ feedback } />
+        <Route exact path= '/coupon' component={ coupon } />
 
         <Route exact path= '/cart' component={ cart } />
         <Route exact path= '/about_us' component={ about_us } />
         <Route exact path= '/product' component={product} />
         <Route exact path= '/productPage' component={productPage} />
-        <Route exact path= '/register' component={ register } />
+
         <Route exact path='/contactus' component={ Contactus } />
 
       </Switch>
